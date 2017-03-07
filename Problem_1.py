0c8f70dbf699603b5dede27de4af99086e43e7c1
cwz <cwbzbupt@163.com>
2017-03-07 21:47:44 +0800

    完善第一题

--- Problem_1.py
+++ Problem_1.py
@@ -1,1 +1,31 @@
-#coding=utf-8
+# coding=utf-8
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+
+       """
+
+        dictionary = {}
+        index = 0
+        for i in nums:
+            j = target - i
+            if dictionary.has_key(j):
+                '''if we find j,we print the index'''
+                List = []
+                List.append(dictionary[j])
+                dictionary[i] = index
+                List.append(dictionary[i])
+                return List
+            else:
+                dictionary[i] = index
+            """
+
+            if we don't find the matched elements, we add the current
+            into the dictionary
+
+            """
+
+            index += 1
